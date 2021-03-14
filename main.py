432c7b1ad7ae54a3ac582c263c8f6d7a316a9ba8
Jason <jasonetienne1@gmail.com>
2021-03-14 17:36:49 +0000

    more Tkinter practise

--- main.py
+++ main.py
@@ -340,7 +340,22 @@ root.mainloop()
 
 ####################### LESSON 13 - Shapes and Graphics
 
+from tkinter import *
 
+root = Tk()
+
+canvas = Canvas(root, width=200, height=100)
+canvas.pack()
+
+blackLine = canvas.create_line(0, 0, 200, 50)  # x & y cords of point 1 & 2 of line
+redLine = canvas.create_line(0, 100, 200, 50, fill='red')
+
+greenBox = canvas.create_rectangle(25, 25, 130, 60, fill='green') #topleft, width, height
+
+# Delete graphics
+canvas.delete(redLine)
+
+root.mainloop()
 
 
 
